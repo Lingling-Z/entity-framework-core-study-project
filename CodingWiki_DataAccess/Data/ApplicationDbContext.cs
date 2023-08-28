fa4cac2cc299a0807a8ca2c228acccd6efb52d47
Lingling-Z <97594629+Lingling-Z@users.noreply.github.com>
2023-08-28 14:09:38 +1200

    initial commit

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        DbSet<Book> Books { get; set; }
+        DbSet<Genre> Genre { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=CodingWiki;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, Title = "Spider without Duty", ISBN = "123B12", Price = 10.99 },
+                new Book { BookId = 2, Title = "Fortune of time", ISBN = "12123B12", Price = 11.99 },
+                new Book { BookId = 3, Title = "Fake Sunday", ISBN = "77652", Price = 20.99 },
+                new Book { BookId = 4, Title = "Cookie Jar", ISBN = "CC12B12", Price = 25.99 },
+                new Book { BookId = 5, Title = "Cloudy Forest", ISBN = "90392B33", Price = 40.99 }
+                );
+        }
+    }
+}
---
+++ CodingWiki_DataAccess/Migrations/20230828015533_SeedBookTable.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace CodingWiki_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBookTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "ISBN", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "123B12", 10.99, "Spider without Duty" },
+                    { 2, "12123B12", 11.99, "Fortune of time" },
+                    { 3, "77652", 20.989999999999998, "Fake Sunday" },
+                    { 4, "CC12B12", 25.989999999999998, "Cookie Jar" },
+                    { 5, "90392B33", 40.990000000000002, "Cloudy Forest" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Genre",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ISBN",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
